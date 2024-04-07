09f4dd9846fbf0447a5779012b64d2e289299bd9
JakeRead-GH <jakeread99@gmail.com>
2024-04-06 21:21:49 -0400

    Set up general KDC methods. Haven't tested them yet since we need endpoints, but they're theoretically sound

--- app/src/main/java/com/example/chatapp/Backend/KDC/CreateKeys.java
+++ app/src/main/java/com/example/chatapp/Backend/KDC/CreateKeys.java
@@ -1,9 +1,28 @@
 package com.example.chatapp.Backend.KDC;
 
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class CreateKeys {
     public Key generateKey() {
+        try {
+            // Initialize the KeyGenerator
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(256); // Specify the key size
+
+            // Generate AES Key
+            SecretKey secretKey = keyGen.generateKey();
+
+            System.out.println("AES Secret Key: " + secretKey);
+
+            return secretKey;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
