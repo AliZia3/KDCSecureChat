5c7f738f082691d3ae7238d44c7c6e9d841caa95
JakeRead-GH <jakeread99@gmail.com>
2024-04-07 23:48:20 -0400

    Didn't finish, added explanation of what we would have done

--- app/src/main/java/com/example/chatapp/Backend/KDC/UpdateKeys.java
+++ app/src/main/java/com/example/chatapp/Backend/KDC/UpdateKeys.java
@@ -5,6 +5,14 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class UpdateKeys {
     public void updateKeys() {
-
+        // TODO: Implement key update logic
+        /* This will update every key in the KDC simultaneously with a new set of keys.
+         * This is done to ensure that the keys are not compromised.
+         * This method will be called periodically to update the keys.
+         *
+         * Ran out of time to implement this unfortunately, so for now keys are static. We would have used
+         * the subscribe notify pattern to allow chats to subscribe to the KDC and be notified when the keys update
+         * to get the new keys.
+         */
     }
 }
